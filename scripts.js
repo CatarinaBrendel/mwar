8153cb578cc7e523670ec59763c3731f7c70e9ff
Catarina Brendel <brendel.catarina@gmail.com>
2019-05-05 14:33:48 +0200

    insert lists

--- scripts.js
+++ scripts.js
@@ -1,10 +1,38 @@
-const sideMenu = document.getElementById('side-menu');
-const userLogo = document.getElementById('user-logo');
+const form = document.getElementById('register');
+const input = form.querySelector('textarea');
 
-userLogo.addEventListener('click', () => {
-    if (sideMenu.style.display == 'none'){
-        sideMenu.style.display = 'block';
-    } else {
-        sideMenu.style.display = 'none';
-    }
-})
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = '';
+    const ul = document.getElementById('message-card');
+    const li = document.createElement('li');
+    li.textContent = text;
+    ul.appendChild(li);
+});
+
+
+
+
+
+
+
+
+
+
+// function messageCharacterCounter (){
+//     let msg = document.getElementsByClassName('message-box');
+//     let charLeftLabel = 'char-left';
+//     let charLeft = document.getElementById('char-left');
+//     let maxChar = 250;
+//     let maxCharWarn = 20;
+
+//     charLeftLabel.textContent = maxChar;
+    
+//     msg.addEventListener('keydown', () => {
+//         console.log(msg.value.length);
+//     });
+// }
+
+// messageCharacterCounter();
+
