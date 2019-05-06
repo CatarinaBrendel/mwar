27fc15633c26ccac789de4809ec694bbb74fa9d5
Catarina Brendel <brendel.catarina@gmail.com>
2019-05-06 17:01:09 +0200

    updated messages

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('register');
 const input = form.querySelector('textarea');
+let postCounter = 0;
 
 function messageWordCounter (){
     let msg = document.getElementsByClassName('message-box')[0];
@@ -22,9 +23,23 @@ form.addEventListener('submit', (e) => {
     input.value = '';
     const ul = document.getElementById('message-card');
     const li = document.createElement('li');
+    const dateLine = document.createElement('p');
+    let getDate = new Date();
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+    const postCounting = document.getElementById('post-counter');
+    
+    dateLine.textContent = getDate.toLocaleTimeString('en-DE', options);
+    
     li.textContent = text;
-    ul.appendChild(li);
+    if (li.textContent !== ''){
+        ul.appendChild(li);
+        li.appendChild(dateLine);
+        postCounter += 1;
+        postCounting.textContent = postCounter + ' Posts';
+    }
 });
+
+
 
 messageWordCounter();
 
