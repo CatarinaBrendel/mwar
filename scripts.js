35020070d2e2b111c881566861ca3935166a8bcf
katebrend <42495865+CatarinaBrendel@users.noreply.github.com>
2019-05-09 16:26:35 +0200

    small designs improvement (#2)
    * ver1.1
    
    * ver1.1
    
    * ver1.1
    
    * with 2nd page
    
    * new files
    
    * layouts addons
    
    * insert lists
    
    * word counter
    
    * updated messages
    
    * changes in the layout
    
    * small designs improvement

---
+++ scripts.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('register');
+const input = form.querySelector('textarea');
+let charCounter = document.getElementsByClassName('char-left')[0];
+let postCounter = 0;
+
+function messageWordCounter (){
+    let msg = document.getElementsByClassName('message-box')[0];
+    let charLeft;
+    let maxChar = 250;
+
+    charCounter.textContent = maxChar;
+
+    msg.addEventListener('keydown', () => {
+        setTimeout(function (){ 
+            charCounter.textContent = maxChar - msg.value.length;
+        }, 1);
+    })
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = '';
+    const ul = document.getElementById('message-card');
+    const li = document.createElement('li');
+    const dateLine = document.createElement('p');
+    let getDate = new Date();
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+    const postCounting = document.getElementById('post-counter');
+    
+    dateLine.textContent = getDate.toLocaleTimeString('en-DE', options);
+    
+    li.textContent = text;
+    if (li.textContent !== ''){
+        ul.appendChild(li);
+        li.appendChild(dateLine);
+        postCounter += 1;
+        postCounting.textContent = postCounter + ' Posts';
+    }
+});
+
+messageWordCounter();
+
